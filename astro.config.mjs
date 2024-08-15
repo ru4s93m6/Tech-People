4ba43b86823a16079f1caec56a275ed10798048d
ru4s93m6 <ru4s93m6@yahoo.com.tw>
2024-08-16 00:04:56 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig, passthroughImageService } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ru4s93m6.github.io/Tech-People/",
   image: {
-    service: passthroughImageService()
+    service: passthroughImageService(),
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
